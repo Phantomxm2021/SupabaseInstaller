59ef19b701cb2d47fae446cbf91de1e7b08a5416
PhantomsXR <phantomxm2021@gmail.com>
2026-08-27 21:15:26 +0800

    fix: tolerate optional postgres roles

--- internal/templates/self-hosted-v0.8.0/volumes/db/roles.sql
+++ internal/templates/self-hosted-v0.8.0/volumes/db/roles.sql
@@ -2,23 +2,14 @@
 \set pgpass `echo "$POSTGRES_PASSWORD"`
 
 -- Optional services are disabled in some presets, so their roles may not
--- exist. Update only roles present in this database.
-DO $$
-DECLARE
-  role_name text;
-BEGIN
-  FOR role_name IN
-    SELECT rolname
-    FROM pg_roles
-    WHERE rolname IN (
-      'authenticator',
-      'pgbouncer',
-      'supabase_auth_admin',
-      'supabase_functions_admin',
-      'supabase_storage_admin'
-    )
-  LOOP
-    EXECUTE format('ALTER ROLE %I WITH PASSWORD %L', role_name, :'pgpass');
-  END LOOP;
-END
-$$;
+-- exist. Generate statements only for roles present in this database.
+SELECT format('ALTER ROLE %I WITH PASSWORD %L;', rolname, :'pgpass')
+FROM pg_roles
+WHERE rolname IN (
+  'authenticator',
+  'pgbouncer',
+  'supabase_auth_admin',
+  'supabase_functions_admin',
+  'supabase_storage_admin'
+)
+\gexec
